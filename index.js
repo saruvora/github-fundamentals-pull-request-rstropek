2cfa15dca2f4050a87bd1c38c588e564024fbb79
saruvora <saransh.vora@gmail.com>
2020-06-24 12:01:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ $(() => {
 
         // You can specify inline conversions
         { from: 'cm', to: 'm', convertFunc: value => value / 100 },
+        { from: 'ounce', to: 'gm', convertFunc: value => value *28.3495},
     ];
     
     function fromMeterToCentimeter(value) {
